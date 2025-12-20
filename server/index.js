77beb9688d66755bd55d5161da6c38e8dd5b5cca
Adarsh-Teaching <147122925+adarshtrainer@users.noreply.github.com>
2025-12-20 12:36:56 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import blogRoutes from "./routes/blog.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+const corsPolicy = {
+  origin: "*",
+  credentials: true,
+};
+
+
+// middlewares
+app.use(cors(corsPolicy));
+app.use(express.json());
+app.use("/api/v1/blog", blogRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/user", userRoutes);
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.log("DB Error", error);
+  }
+};
+
+connectDB().then(() => {
+  app.listen(port,'0.0.0.0', () => console.log(`Server running in ${port}`));
+});
